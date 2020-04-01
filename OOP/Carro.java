d3fb2e6dd8c44d911b4ee376cf084bea4dad01cc
gasperpb <achillespb@hotmail.com>
2020-04-01 18:27:58 -0300

    Criado a class carro

---
+++ OOP/Carro.java
@@ -0,0 +1,26 @@
+package OOP;
+
+/**
+ * Aula02
+ *
+ * @author	Vasconcelos Achilles
+ * @since	v0.0.1
+ * @global
+ */
+public class Carro {
+    String marca;
+    String modelo;
+    int ano;
+    String variante;
+
+    public Carro(String marca, String modelo, int ano, String variante) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.variante = variante;
+    }
+
+   
+
+
+}
---
+++ OOP/NumeroPessoa.java
@@ -0,0 +1,11 @@
+package OOP;
+
+/**
+ * NumeroPessoa
+ */
+public class NumeroPessoa {
+
+    public static void main(String[] args) {
+        int NumeroPessoas = new NumeroPessoas();
+    }
+}
